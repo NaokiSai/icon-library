17c012e3476a1152e2cac496fff2ed8f9ec7da1a
NaokiSai <NaokiSai@users.noreply.github.com>
2026-01-18 12:25:57 +0000

    deploy: 7cc46264aafbb06c0eaafae942e4e1e46bcea432

---
+++ assets/Button.stories-O7QGEdUY.js
@@ -0,0 +1,20 @@
+import{B as o}from"./Button-DEXzilFJ.js";import"./iframe-atb9AYJO.js";import"./preload-helper-PPVm8Dsz.js";const{fn:t}=__STORYBOOK_MODULE_TEST__,m={title:"Example/Button",component:o,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{backgroundColor:{control:"color"}},args:{onClick:t()}},r={args:{primary:!1,label:"Button"}},a={args:{label:"Button"}},e={args:{size:"large",label:"Button"}},s={args:{size:"small",label:"Button"}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    primary: false,
+    label: "Button"
+  }
+}`,...r.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Button'
+  }
+}`,...a.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    size: 'large',
+    label: 'Button'
+  }
+}`,...e.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    size: 'small',
+    label: 'Button'
+  }
+}`,...s.parameters?.docs?.source}}};const p=["Primary","Secondary","Large","Small"];export{e as Large,r as Primary,a as Secondary,s as Small,p as __namedExportsOrder,m as default};
